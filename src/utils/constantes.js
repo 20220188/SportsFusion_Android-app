c4795735cc734bf4a761a908088bc575a91a8217
20220188 <107494234+20220188@users.noreply.github.com>
2024-07-06 14:18:36 -0600

    ip

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -3,6 +3,11 @@
 //utilizar la direccion IP del servidor y no localhost
 //export const IP = 'http://10.10.0.165';
 //export const IP = 'http://192.168.137.65';
+
 //IP lab de Will Musun
-export const IP = 'http://10.10.2.108';
+//export const IP = 'http://10.10.2.108';
+
+//IP Musun
+export const IP = 'http://192.168.1.23';
+
 //export const IP = 'http://172.20.10.10';
