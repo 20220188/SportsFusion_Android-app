78fda98b11a78dfca79ac92668f5940ac1f3e6de
20220188 <107494234+20220188@users.noreply.github.com>
2024-07-07 17:30:26 -0600

    .

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -8,9 +8,9 @@
 //export const IP = 'http://10.10.2.108';
 
 //IP Musun
-//export const IP = 'http://192.168.1.23';
+export const IP = 'http://192.168.1.23';
 
 //export const IP = 'http://172.20.10.10';
 
 //IP laptop de Kevin
-export const IP = 'http://192.168.0.12';
+//export const IP = 'http://192.168.0.12';
